eeb82cdf7a621bce2b55757fcb79fdbeb052e545
reda-guemry <guemryreda@gmail.com>
2026-09-23 22:23:31 +0000

    Add pre-trained model results path and update model training notebook to include results saving

--- config/path.py
+++ config/path.py
@@ -4,3 +4,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATA_RAW_PATH = os.path.join(BASE_DIR, "data", "raw", "car-price.csv")
 DATA_PROCESSED_PATH = os.path.join(BASE_DIR, "data", "processed", "voiture_data_cleaned.csv")
+
+PRE_TRAINED_MODEL_RESULTS_PATH = os.path.join(BASE_DIR, "models", 'results' , "pre_trained_model_results.csv")
